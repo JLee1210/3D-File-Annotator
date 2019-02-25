dec567542cddba3a337521882d06012ae6294abc
JLee1210 <JLee1210@users.noreply.github.com>
2019-02-25 08:29:27 -0500

    Update 1
    1. Changed obj uploading process to folder upload
    2. Loading all obj from folder and chose json store file [currently user select obj by obj]
    3. Check if json is exist then fill the label button
    4. Background as white and lighting [good to see]
    5. Add comment box and save as JSON as well
    6. Labeling formate
    {
    'label':parent_class ; child_class
    }
    7. Add all class name from
    https://engineering.purdue.edu/cdesign/wp/wp-content/uploads/2012/10/ESB.pdf
    Table2
    8. Label name example figure like bellow

--- Code/sice/3d.js
+++ Code/sice/3d.js
@@ -1,19 +1,11 @@
 var scene = new THREE.Scene();
-//scene.background = new THREE.Color( 0xffffff );
+scene.background = new THREE.Color(0xF5F5F5);
 var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+var object = new THREE.Object3D();
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( 1000, window.innerHeight );
+
+renderer.setSize( 1095, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
-/*var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-*/
-
-
-//camera.position.z = 1000;
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -23,14 +15,14 @@ controls.enableZoom = true;
 
 controls.update();
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+var keyLight = new THREE.DirectionalLight(new THREE.Color(0xA9A9A9), .5);
+keyLight.position.set(0, 200, 0);
  
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 0, 100);
+var fillLight = new THREE.DirectionalLight(new THREE.Color(0xA9A9A9), 0.75);
+fillLight.position.set(100, 200, 100);
  
-var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
+var backLight = new THREE.DirectionalLight(0xA9A9A9, 1.3);
+backLight.position.set(-100, -200, -100).normalize();
  
 scene.add(keyLight);
 scene.add(fillLight);
@@ -43,110 +35,227 @@ var l = -1;
 var r = 1;
 var name = "";
 var c = 0;
+var file;
 
+//next obj file via right arrow
 document.getElementById('right').onclick = function(){
-	while(scene.children.length > 0){ 
-    	scene.remove(scene.children[0]); 
-	}	
-	scene.add(keyLight);
-	scene.add(fillLight);
-	scene.add(backLight);
-	var file_data = $('input[name="file"]')[0].files;
-	name = file_data[r].name;
-	c = r;
-	fileUpload(name);
-	nameUpdate(name);
-	animate();
-	console.log(r);
-	if(r < file_data.length - 1 && l < file_data.length - 1){
-		r += 1;
-		l += 1;
-	}
-	else if(r < file_data.length - 1 && l == file_data.length - 1){
-		r += 1;
-		l = 0;
-	}
-	else{
-		r = 0;
-		l = file_data.length - 2;
+	if (files.length != 0) {
+		while(scene.children.length > 0){ 
+	    	scene.remove(scene.children[0]); 
+		}	
+		scene.add(keyLight);
+		scene.add(fillLight);
+		scene.add(backLight);
+		name = files[r].name;
+		file = files[r];
+		c = r;
+		fileUpload(file);
+		nameUpdate(name);
+		animate();
+		console.log(r);
+		if(r < files.length - 1 && l < files.length - 1){
+			r += 1;
+			l += 1;
+		}
+		else if(r < files.length - 1 && l == files.length - 1){
+			r += 1;
+			l = 0;
+		}
+		else{
+			r = 0;
+			l = files.length - 2;
+		}
+		$('input:checkbox').removeAttr('checked');
+		document.getElementById('comment').value = "";
 	}
 }
 
+//previous obj file via left arrow
 document.getElementById('left').onclick = function(){
-	while(scene.children.length > 0){ 
-		scene.remove(scene.children[0]); 
-	}	
-	scene.add(keyLight);
-	scene.add(fillLight);
-	scene.add(backLight);
-	var file_data = $('input[name="file"]')[0].files;
-	var l2 = l;
-	console.log(l2);
-	if(l < 0){
-		l = file_data.length - 2;
-		l2 = file_data.length - 1;
-		r = 0;
+	if (files.length != 0) {
+		while(scene.children.length > 0){ 
+			scene.remove(scene.children[0]); 
+		}	
+		lighting(keyLight, fillLight, backLight);
+		var l2 = l;
+		console.log(l2);
+		if(l < 0){
+			l = files.length - 2;
+			l2 = files.length - 1;
+			r = 0;
+		}
+		else if(r == 0){
+			l -= 1;
+			r = files.length - 1;
+		}
+		else{
+			l -= 1;
+			r -= 1;
+		}
+		name = files[l2].name;
+		file = files[l2];
+		fileUpload(file);
+		nameUpdate(name);
+		c = l2;
+		animate();
+		$('input:checkbox').removeAttr('checked');
+		document.getElementById('comment').value = "";
 	}
-	else if(r == 0){
-		l -= 1;
-		r = file_data.length - 1;
-	}
-	else{
-		l -= 1;
-		r -= 1;
-	}
-	name = file_data[l2].name;
-	fileUpload(name);
-	nameUpdate(name);
-	c = l2;
-	animate();
 }
 
+//updates title of obj file
 function nameUpdate(name){
-	document.getElementById("objName").innerHTML = name;
+	document.getElementById("objName").innerHTML = "File Name: " + name;
 }
 
-function fileUpload(name){
+//uploads obj onto canvas
+function fileUpload(file){
 	var objLoader = new THREE.OBJLoader();
-	objLoader.setPath('/sice/objs/');
-	objLoader.load(name, function(object){
-		object.position.y -= 0;
-		object.position.x -= 5;
-		scene.add(object);
-		boundingBox.setFromObject( object );
-	    const center = boundingBox.getCenter();
-	    const size = boundingBox.getSize();
-		const maxDim = Math.max( size.x, size.y, size.z );
-		const fov = camera.fov * ( Math.PI / 180 );
-	    let cameraZ = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
-		cameraZ *= offset;
-		const minZ = boundingBox.min.z;
-	    const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
-		camera.position.z = center.z + cameraZ;
-		controls.target = center;
+      
+    // set your file encoding
+    var encoding = 'ISO-8859-1'; 
+	var jreader = new FileReader();
+    var json = null;
+    var jsonFile = "";
+    var objFile = "";
+    var sice = false;
+    if (file != null) {
+	    // set on load handler for reader
+	    	for (var i = 0; i < jsons.length; i++) {
+	    		jsonFile = jsons[i].name.split(".")[0];
+	    		objFile = file.name.split(".")[0];
+	    		console.log("json: " + jsonFile + "\nobj : " + objFile);
+	    		if (jsonFile === objFile) { //if json file exists, read the file
+	    			jsonFile = jsons[i];
+	    			jreader.onload = function(e) {
+	    				json = jreader.result;
+	    			}
+	    			sice = true;
+	    			break;
+	    		}
+	    	}
+	    if (sice == true)
+	  		jreader.readAsText(jsonFile, encoding);
 
-	    // prevent camera from zooming out far enough to create far plane cutoff
-	    controls.maxDistance = cameraToFarEdge * 2;
-	    controls.saveState();
-	});
-	animate();
+    // create a file reader
+    var reader = new FileReader();
+    // set on load handler for reader
+    reader.onload = function(e) {
+    	if (sice == true) {
+	    	var data = JSON.parse(json);
+		    document.getElementById("user").innerHTML = data.user;
+		    document.getElementById(data.label).checked = true;
+		    $(':checkbox').not( document.getElementById(data.label) ).attr('checked', false);
+		    document.getElementById("date").value = data.date;
+		    document.getElementById("comment").value = data.comment;
+		}
+		    var result = reader.result;
+			lighting(keyLight, fillLight, backLight);
+		    // parse using your corresponding loader
+		    object = objLoader.parse( result );
+		    object.position.y -= 0;
+			object.position.x -= 5;
+			scene.add(object);
+			boundingBox.setFromObject( object );
+		    const center = boundingBox.getCenter();
+		    const size = boundingBox.getSize();
+			const maxDim = Math.max( size.x, size.y, size.z );
+			const fov = camera.fov * ( Math.PI / 180 );
+		    let cameraZ = Math.abs( maxDim / 4 * Math.tan( fov * 2 ) );
+			cameraZ *= offset;
+			const minZ = boundingBox.min.z;
+		    const cameraToFarEdge = ( minZ < 0 ) ? -minZ + cameraZ : cameraZ - minZ;
+			camera.position.z = center.z + cameraZ;
+			controls.target = center;
+
+		    // prevent camera from zooming out far enough to create far plane cutoff
+		    controls.maxDistance = cameraToFarEdge * 2;
+		    controls.saveState();
+  		}
+
+	    // read the file as text using the reader
+	    reader.readAsText(file, encoding);
+	    animate();
+	    name = file.name
+	    nameUpdate(name);
+	}
 }
+var files = []; //array of files that are .obj
+var jsons = []; //json files
 
-document.getElementById('sice').onclick = function(){
+window.onload = function() {
+	var jsonInput = document.getElementById('jFile');
+	jsonInput.addEventListener('change', function(e) {
+		jsons = $('input[name="jFile"]')[0].files;
+		console.log(jsons);
+	});
+
+	var fileInput = document.getElementById('file');
+	fileInput.addEventListener('change', function(e) {
 	l = -1;
 	r = 1;
 	c = 0;
-	var file_data = $('input[name="file"]')[0].files;
-	name = file_data[0].name;
+	files = [];
 
-	fileUpload(file_data[0].name);
-	nameUpdate(file_data[0].name);
-	/*var objLoader = new THREE.OBJLoader();
-	objLoader.setPath('/sice/objs/');
-	objLoader.load($('input[name="file"]')[0].files[0].name, function(object){
-		object.position.y -= 0;
+	//reset scene 
+	while(scene.children.length > 0){ 
+		scene.remove(scene.children[0]); 
+	}	
+	var file_data = $('input[name="file"]')[0].files;
+	
+	//filter out all the non-obj files
+	for (var i = 0; i < file_data.length; i++) {
+		if (file_data[i].name.split(".")[file_data[i].name.split(".").length - 1] == "obj") {
+			files.push(file_data[i]);
+		}
+	}
+	var objLoader = new THREE.OBJLoader();
+	var i = 0;
+	file = files[i];
+      
+    // set your file encoding
+    var encoding = 'ISO-8859-1'; 
+
+    // create a file reader
+    var reader = new FileReader();
+    var jreader = new FileReader();
+    var json = "";
+    var jsonFile = "";
+    var objFile = "";
+    var sice = false;
+    if (file != null) {
+	    // set on load handler for reader
+	    	for (var i = 0; i < jsons.length; i++) {
+	    		jsonFile = jsons[i].name.split(".")[0];
+	    		objFile = file.name.split(".")[0];
+	    		console.log("json: " + jsonFile + "\nobj : " + objFile);
+	    		if (jsonFile === objFile) { //if json file exists, read the file
+	    			jsonFile = jsons[i];
+	    			jreader.onload = function(e) {
+	    				json = jreader.result;
+	    			}
+	    			sice = true;
+	    			break;
+	    		}
+	    	}
+	  	jreader.readAsText(jsonFile, encoding);
+
+	    reader.onload = function(e) {
+	   	var data = JSON.parse(json);
+	    document.getElementById("user").innerHTML = data.user;
+	    document.getElementById(data.label).checked = true;
+	    $(':checkbox').not( document.getElementById(data.label) ).attr('checked', false);
+	    document.getElementById("date").value = data.date;
+	    document.getElementById("comment").value = data.comment;
+	    
+	    var result = reader.result;
+		lighting(keyLight, fillLight, backLight);
+        // parse using your corresponding loader
+        object = objLoader.parse( result );
+        object.position.y -= 0;
 		object.position.x -= 5;
 		scene.add(object);
+		//centers obj object
 		boundingBox.setFromObject( object );
 	    const center = boundingBox.getCenter();
 	    const size = boundingBox.getSize();
@@ -162,8 +271,22 @@ document.getElementById('sice').onclick = function(){
 	    // prevent camera from zooming out far enough to create far plane cutoff
 	    controls.maxDistance = cameraToFarEdge * 2;
 	    controls.saveState();
-	});*/
+      }
+
+      // read the file as text using the reader
+      reader.readAsText(file, encoding);
+      animate();
+      name = file.name
+      nameUpdate(name);
+      $('input:checkbox').removeAttr('checked');
+      document.getElementById('comment').value = "";
+	}
+	  else //if file isn't obj, alert user
+	  	alert("File is not '.obj', please try again.");
+
+    });
 }
+
 var animate = function () {
 	requestAnimationFrame( animate );
 
@@ -173,13 +296,20 @@ var animate = function () {
 	renderer.render(scene, camera);
 };
 
-animate();
+//convenient source for debugging lighting
+var lighting = function (keyLight, fillLight, backLight) {
+	scene.add(keyLight);
+	scene.add(fillLight);
+	scene.add(backLight);
+}
 
+//sets date to current date
 document.addEventListener("DOMContentLoaded", function(event) {
 	let today = new Date().toISOString().substr(0, 10);
 	document.querySelector("#date").value = today;
 });
 
+//gets formatted date for JSON file
 function getFormattedDate(date) {
     return date.getFullYear()
         + "-"
@@ -193,8 +323,7 @@ $(document).ready(function() {
 	    var output = {};
 
 	    //file json
-	    var file_data = $('input[name="file"]')[0].files;
-		    output["file"] = file_data[c].name;
+		    output["file"] = files[c].name;
 
 		//form json
 		jQuery("#myform").serializeArray().map(function(item) {
@@ -216,7 +345,17 @@ $(document).ready(function() {
 		    a.download = fileName;
 		    a.click();
 		}
-		download(output, 'data-' + name + '.json', 'application/json');
+		download(output, name.split(".")[0] + '.json', 'application/json');
 		$('input:checkbox').removeAttr('checked');
+		document.getElementById('comment').value = "";
 	});
 });
+
+$('.labels input:checkbox').click(function(){
+    var $inputs = $('.labels input:checkbox'); 
+    if($(this).is(':checked')){  // <-- check if clicked box is currently checked
+       $inputs.not(this).prop('disabled',true); // <-- disable all but checked checkbox
+    }else{  //<-- if checkbox was unchecked
+       $inputs.prop('disabled',false); // <-- enable all checkboxes
+    }
+})
