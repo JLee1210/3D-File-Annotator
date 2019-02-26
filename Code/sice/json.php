9156504db21fce4638badb5f1cafd3368f877ce0
JLee1210 <JLee1210@users.noreply.github.com>
2019-02-26 12:54:47 -0500

    changed json.php
    works without json folder

--- Code/sice/json.php
+++ Code/sice/json.php
@@ -1,6 +1,10 @@
 <?php 
 	//if(isset($_POST['submit'])) {
 	  $file = "json/" . $_POST['fname'] . ".json";
+	  if (!file_exists($path)) 
+		{
+		    mkdir('json/', 0777, true);
+		}
 	  $arr = array('user' => $_POST['user'], 'file' => $_POST['file'], 'label' => $_POST['label'], 'date' => $_POST['date'], 'comment' => $_POST['comment']);
 	  $json_string = json_encode($arr, JSON_PRETTY_PRINT);
 	  file_put_contents($file, $json_string);
